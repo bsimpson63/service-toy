9e0b6e6778a147421ea49f58e355184dc6d12ec7
Brian Simpson <bsimpson63@gmail.com>
2015-11-10 10:49:41 -0800

    Add some toy functions.

--- reddit_service_toy/__init__.py
+++ reddit_service_toy/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import random
 
 from baseplate import make_metrics_client, config, diagnostics
 from baseplate.integration.thrift import BaseplateProcessorEventHandler
@@ -10,19 +11,25 @@ logger = logging.getLogger(__name__)
 
 
 class Handler(ToyService.ContextIface):
-    def __init__(self):
-        pass
+    def __init__(self, real_random=False):
+        self.real_random = real_random
 
     def is_healthy(self, context):
-        # TODO: check that your service has everything it needs to to function
         return True
 
-    # TODO: implement your service here!
+    def get_random(self, context):
+        if self.real_random:
+            return random.randint(0, 100)
+        else:
+            return 5
+
+    def multiply(self, context, one, two):
+        return one * two
 
 
 def make_processor(app_config):
     cfg = config.parse_config(app_config, {
-        # TODO: add your config spec here
+        "real_random": config.Boolean,
     })
 
     metrics_client = make_metrics_client(app_config)
@@ -31,7 +38,7 @@ def make_processor(app_config):
     agent.register(diagnostics.LoggingDiagnosticsObserver())
     agent.register(diagnostics.MetricsDiagnosticsObserver(metrics_client))
 
-    handler = Handler()
+    handler = Handler(real_random=cfg.real_random)
     processor = ToyService.ContextProcessor(handler)
     event_handler = BaseplateProcessorEventHandler(logger, agent)
     processor.setEventHandler(event_handler)
--- reddit_service_toy/toy.thrift
+++ reddit_service_toy/toy.thrift
@@ -1,5 +1,6 @@
 include "baseplate.thrift"
 
 service ToyService extends baseplate.BaseplateService {
-
+  i32 get_random(),
+  i32 multiply(1:i32 one, 2:i32 two),
 }
